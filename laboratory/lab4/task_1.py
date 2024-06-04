9c5627712a2e4049589d470479df88679afb77ba
sauce-chili <saucechilidev@gmail.com>
2024-06-05 00:26:22 +0300

    Fix bug with visualisation graphics

--- laboratory/lab4/task_1.py
+++ laboratory/lab4/task_1.py
@@ -15,6 +15,7 @@ from PyQt5.QtWidgets import (
     QAbstractItemView
 )
 
+
 class Task1(TaskView):
     def __init__(self, task_number: int, parent):
         super().__init__(parent)
@@ -26,17 +27,18 @@ class Task1(TaskView):
         self.calculateButton.clicked.connect(self.load_data)
         self.table.setRowCount(2)
         self.table.setVerticalHeaderLabels(['xi', 'pi'])
+        self.prepare_plots()
 
-    def prepare_plots(self):
+    def prepare_plots(self):  # o
+        lt = QVBoxLayout()
+        self.distribution_polygon_plot = Polygon(self, name='Многоугольник распределения')  # ака полигон частот
+        lt.addWidget(self.distribution_polygon_plot, alignment=Qt.Alignment())
+        self.distribution_polygon.setLayout(lt)
+
         lt = QVBoxLayout()
         self.discrete_function_graph = Function(self, name='Эмпирическая функция распределения')
         lt.addWidget(self.discrete_function_graph, alignment=Qt.Alignment())
         self.discrete_function_plot.setLayout(lt)
-
-        lt = QVBoxLayout()
-        self.distribution_polygon_plot = Polygon(self, name='Многоугольник распределения')
-        lt.addWidget(self.distribution_polygon_plot, alignment=Qt.Alignment())
-        self.distribution_polygon.setLayout(lt)
 
     def load_data(self):
         x_count = self.xCount.value()
@@ -62,27 +64,27 @@ class Task1(TaskView):
             table.setItem(1, i, QTableWidgetItem(f'{P}\t'))
 
         # Основные харрактеристики
-        self.m_label.setText(str(round(distribution_data.x_v, 5)))
-        self.d_label.setText(str(round(distribution_data.D_v, 5)))
+        self.m_label.setText(str(round(distribution_data.M, 5)))
+        self.d_label.setText(str(round(distribution_data.D, 5)))
         self.sigma_label.setText(str(round(distribution_data.sigma, 5)))
 
-        allM0 = ""
-        for i in distribution_data.M0:
-            allM0 += str(round(i, 5)) + ", "
-        allM0 = allM0[:-2]
+        allM0 = ", ".join(map(lambda t: str(round(t, 5)), distribution_data.M0))
 
         self.m0_label.setText(allM0)
 
+        plot_data = process_distribution_plot_data(distribution_data)
+
         # Функция распределения
-        plot_data = process_discrete_plot_data
-        self.functionText.setPlainText(plot_data['F'])
+        self.functionText.setPlainText(plot_data['F*'])
         self.discrete_function_graph.display(plot_data['func'], 'x', 'F*(x)', color='k')
 
         # Гистограмма частот и относительных частот
-        plot_data = prepare_distribution_polygon_plots(distribution_data.X, distribution_data.P)
-        self.density_graph.display(
-            plot_data, 'x', 'f*(x)', color='k'
-        )
+        self.distribution_polygon_plot.set_x_gap(None)
+        self.distribution_polygon_plot.display(distribution_data.X, distribution_data.P, 'x', 'p')
+
+        # self.density_graph.display(
+        #     plot_data, 'x', 'F*(x)', color='k'
+        # )
 
     def read_manual_data(self):
         count = self.xCount.value()
@@ -90,13 +92,13 @@ class Task1(TaskView):
 
         for i in range(count):
             x = self.table.item(0, i)
-            x = x.value()
+            x = int(x.text())
 
             probability = self.table.item(1, i)
             if probability is None:
                 raise ValueError('Не указана вероятность для ' + str(i + 1) + '-й точки.')
 
-            probability = probability.value()
+            probability = float(probability.text())
             data.X.append(x)
             data.P.append(probability)
 
